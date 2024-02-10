360df1ef73a6e2c7ea5fb73f4eec8839c4327362
Stephen Gabriel S. Alojado <157614863+t1pen@users.noreply.github.com>
2024-02-10 23:36:38 +0800

    Add files via upload

---
+++ CARTESIAN_CEILING.m
@@ -0,0 +1,35 @@
+    disp("Cartesian_Ceiling")
+syms a1 a2 a3 a4 d1 d2 d3
+
+%% Link Lengths
+a1 = 3;
+a2 = 5;
+a3 = 5;
+a4 = 3;
+
+%% Joint Variables
+d1 = 5;
+d2 = 5;
+d3 = 5;
+
+%% D-H Parameters: [theta, d, r, alpha, offset]
+% if prismatic joint: theta = theta, d = 0, offset = 1, after offset put the value of d
+% if revolute joint: theta = 0, offset = 0, after offset put the value of theta
+
+H0_1 = Link([0,0,0,pi/2,1,-a1]);
+H0_1.qlim = [0 0];
+
+H1_2 = Link([pi/2,0,0,pi/2,1,a2]);
+H1_2.qlim = [0 d1];
+
+H2_3 = Link([pi/2,0,0,3*pi/2,1,a3]);
+H2_3.qlim = [0 d2];
+
+H3_4 = Link([0,0,0,0,1,a4]);
+H3_4.qlim = [0 d3];
+
+%% Build Mechanical Manipulator
+
+Cart_Inverse = SerialLink([H0_1 H1_2 H2_3 H3_4], 'name', 'Inverse Cart')
+Cart_Inverse.plot([0 0 0 0], 'workspace', [-5 15 -15 5 -15 15])
+Cart_Inverse.teach
---
+++ MODERN_SPHERICAL.m
@@ -0,0 +1,36 @@
+disp("Spherical Modern Variant")
+
+syms a1 a2 a3 t1 t2 d3
+
+%% Link Lengths
+
+a1 = 10;
+a2 = 10;
+a3 = 5;
+
+%% Joint Variables
+t1 = 0;
+t2 = 0;
+d3 = 5;
+
+%% D-H Parameters: [theta, d, r, alpha, offset]
+% if prismatic joint: theta = theta, d = 0, offset = 1, after offset put the value of d
+% if revolute joint: theta = 0, offset = 0, after offset put the value of theta
+
+H0_1 = Link([0,a1,0,pi/2,0,t1]);
+H0_1.qlim = pi/180*[-90 90];
+
+H1_2 = Link([0,0,a2,pi/2,0,t2]);
+H1_2.qlim = pi/180*[0 90];
+
+H2_3 = Link([0,0,0,0,1,a3]);
+H2_3.qlim = [0 d3];
+
+%% Build Mechanical Manipulator
+
+Mod_Sphere = SerialLink([H0_1 H1_2 H2_3], 'name', 'RRP')
+Mod_Sphere.plot([0 0 0], 'workspace', [-20 20 -20 20 -25 30])
+Mod_Sphere.teach
+
+
+
